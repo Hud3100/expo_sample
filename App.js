9f8b4c54c63f74a6dfb16ef05972c3ad2dffc29e
Hiroaki <hud3100@gmail.com>
2020-12-13 11:59:11 +0900

    プログラムの初期化

--- App.js
+++ App.js
@@ -1,14 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+export default class App extends React.Component {
+
+  state = {
+    text1 : "placeholder"
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>入力してください</Text>
+        <TextInput
+          onChangeText = {(t) => this.setState({text1:t})}
+        />
+        <Button
+          title="Entry"
+          onPress={this.entry}
+        />
+      </View>
+    );
+  }
+
+  entry = () => {
+    alert(this.state.text1);
+  }
 }
 
 const styles = StyleSheet.create({
