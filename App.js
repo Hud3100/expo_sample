35ca17559d6d63697a5891e114fe7198be64df7b
Hiroaki <hud3100@gmail.com>
2021-01-30 14:09:14 +0900

    TextをViewとして保存する機能を追加

--- App.js
+++ App.js
@@ -1,38 +1,113 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import React, {useState, useRef} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
+import ViewShot, {captureRef} from "react-native-view-shot";
 
+const App = () => {
+  const [imageURI, setImageURI] = useState('https://raw.githubusercontent.com/AboutReact/sampleresource/master/sample_img.png',);
+  const [savedImagePath, setSavedImagePath] = useState('');
 
-export default class App extends React.Component {
+  const takeScreenShot = async () => {
+    try {
+      const uri = await captureRef(viewRef, {
+        format: 'jpg',
+        quality: 0.8,
+      });
+      console.log(uri);
+      setSavedImagePath(uri);
+      setImageURI(uri);
+    } catch (error) {
+      console.log('error', error);
+    }
+    // console.log(viewRef);
+    // testRef.capture().then(
+    //   (uri) => {
+    //     console.log("success")
+    //     console.log(uri);
+    //     setSavedImagePath(uri);
+    //     setImageURI(uri);
+    //   },
+    //   (error) => console.error('Oops, Something Went Wrong', error),
+    // );
+  };
 
-  state = {
-    text1 : "placeholder"
-  }
+  const viewRef = useRef();
 
-  render() {
-    return (
+  return (
+    <SafeAreaView style={{flex: 1}}>
       <View style={styles.container}>
-        <Text>入力してください</Text>
-        <TextInput
-          onChangeText = {(t) => this.setState({text1:t})}
+        <Image
+          source={{uri: imageURI}}
+          style={{
+            width: 200,
+            height: 300,
+            resizeMode: 'contain',
+            marginTop: 5
+          }}
         />
-        <Button
-          title="Entry"
-          onPress={this.entry}
-        />
-      </View>
-    );
-  }
+        <ViewShot
+          style={styles.container}
+          ref={viewRef}
+          options={{
+            format: 'jpg',
+            quality: 0.8,
+          }}
+        >
+          <Text>Test Shot</Text>
+        </ViewShot>
 
-  entry = () => {
-    alert(this.state.text1);
-  }
-}
+        <TouchableOpacity
+          style={styles.buttonStyle}
+          onPress={takeScreenShot}
+        >
+          <Text style={styles.buttonTextStyle}>
+            Take ScreenShot
+          </Text>
+          <Text style={styles.textStyle}>
+            {
+              savedImagePath ? `Saved Image Path\n ${savedImagePath}` : ''
+            }
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  )
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
+    padding: 10,
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  titleText: {
+    fontSize: 22,
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+  textStyle: {
+    textAlign: 'center',
+    padding: 10,
+  },
+  buttonStyle: {
+    fontSize: 16,
+    color: 'white',
+    backgroundColor: 'green',
+    padding: 5,
+    minWidth: 250,
+  },
+  buttonTextStyle: {
+    padding: 5,
+    color: 'white',
+    textAlign: 'center',
   },
 });
