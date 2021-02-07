126fdee55a7d2c9091af036d743460bb2d550098
Hiroaki <hud3100@gmail.com>
2021-02-07 12:14:23 +0900

    タブバーにアイコンを導入

--- Router.js
+++ Router.js
@@ -1,45 +1,62 @@
 import React from 'react';
 import { Scene, Router, Tabs, Actions } from 'react-native-router-flux';
+import { StyleSheet } from 'react-native';
 import pageA from './screens/pageA';
 import recordComponent from './screens/kotobaRecord';
 import loginScreen from './screens/loginScreen';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const RouterComponent = () => {
   return (
     <Router>
       <Tabs
         key='root'
+        showLabel={false}
         swipeEnabled={ true }
         animationEnabled={ false }
-        tabBarStyle={{backgroundColor:'#F8F8F8'}}
+        tabBarStyle={styles.tabBar}
       >
-            <Scene
-              key="pageA"
-              component={pageA}
-              tabBarLabel='ログイン'
-              title="pageA"
-              // rightTitle="ログイン画面へ"
-              // onRight={(//) => { Actions.loginScreen(); }}
-            />
-            <Scene
-              key="loginScreen"
-
-              component={loginScreen}
-              tabBarLabel='ログイン'
-              title="ログイン画面"
-              // rightTitle="言葉を残す"
-              // onRight={() => { Actions.recordComponent(); }}
-            />
-            <Scene
-              initial={ true }
-              key="recordComponent"
-              tabBarLabel='ログイン'
-              component={recordComponent}
-              title="言葉を残す"
-            />
+        <Scene
+          key="pageA"
+          component={pageA}
+          title="pageA"
+          icon={() => (
+            <Icon
+              name="plus-circle"
+              size={30}
+              color="#f3a68c"
+              />)}
+          // rightTitle="ログイン画面へ"
+          // onRight={(//) => { Actions.loginScreen(); }
+        />
+        <Scene
+          key="loginScreen"
+          component={loginScreen}
+          tabBarLabel='ログイン'
+          title="ログイン画面"
+          // rightTitle="言葉を残す"
+          // onRight={() => { Actions.recordComponent(); }}
+        />
+        <Scene
+          initial={ true }
+          key="recordComponent"
+          tabBarLabel='ログイン'
+          component={recordComponent}
+          title="言葉を残す"
+        />
       </Tabs>
     </Router>
   )
 }
 
 export default RouterComponent;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  tabBar: {
+    borderWidth: 0,
+    backgroundColor:'#FFF'
+  }
+});
